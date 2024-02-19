0c48da787a3c25905d2613624bc46af515a2d7ab
henry-49 <henryswebdevelopment@gmail.com>
2024-02-19 15:03:38 +0100

    Add function to Redisplay the Form with the Error Message for Each Field

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -87,9 +87,20 @@ class Products
             "description" => empty($_POST["description"]) ? null : $_POST["description"],
         ];
 
-        var_dump($this->model->insert($data));
+        if ($this->model->insert($data)){
+            
+            echo "record successfully created.";
+            
+        }else{
 
-        print_r($this->model->getErrors());
+            echo $this->viewer->render("shared/header.php", [
+                "title" => "New Product"
+            ]);
+
+            echo  $this->viewer->render("Products/new.php", [
+                "errors" => $this->model->getErrors()
+            ]);
+        }
         
     }
 }
--- views/Products/new.php
+++ views/Products/new.php
@@ -4,6 +4,10 @@
     <label for="name">Name</label>
     <input type="text" name="name" id="name" />
 
+    <?php if (isset($errors["name"])) : ?>
+    <p><?= $errors["name"] ?></p>
+    <?php endif; ?>
+
     <label for="description">Description</label>
     <textarea name="description" id="description" rows="3"></textarea>
 
